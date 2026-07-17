0b2c36b53e9cda3529a7146c68d2bae034865123
KLMNBRD228\blues <blues.wang@kalerm.com>
2026-07-17 15:02:38 +0800

    Improve blog filtering and refresh links and tools navigation

--- src/data/links.ts
+++ src/data/links.ts
@@ -10,27 +10,41 @@ export interface LinkGroup {
 
 export const LINK_GROUPS: LinkGroup[] = [
   {
-    title: "开发与文档",
+    title: "通用及办公",
     links: [
-      { name: "GitHub", href: "https://github.com" },
-      { name: "Astro Docs", href: "https://docs.astro.build" },
-      { name: "MDN Web Docs", href: "https://developer.mozilla.org" }
+      { name: "立创商城", href: "https://www.szlcsc.com/" },
+      { name: "淘宝", href: "https://www.taobao.com/" },
+      { name: "京东", href: "https://www.jd.com/" },
+      { name: "Google", href: "https://www.google.com/" },
+      { name: "维基百科", href: "https://www.wikipedia.org/" },
+      { name: "bilibili", href: "https://www.bilibili.com/" },
+      { name: "YouTube", href: "https://www.youtube.com/" },
+      { name: "GitHub", href: "https://github.com/" },
+      { name: "Gitee", href: "https://gitee.com/" },
+      { name: "飞书云文档", href: "https://my.feishu.cn/" },
+      { name: "知乎", href: "https://www.zhihu.com/" },
+      { name: "SmallPDF", href: "https://smallpdf.com/cn#r=app" }
     ]
   },
   {
     title: "电子硬件",
     links: [
-      { name: "立创商城", href: "https://www.szlcsc.com" },
-      { name: "嘉立创 EDA", href: "https://lceda.cn" },
-      { name: "DigiKey", href: "https://www.digikey.cn" }
+      { name: "Ti官网", href: "https://www.ti.com/" },
+      { name: "ST官网", href: "https://www.st.com/" },
+      { name: "ADI官网", href: "https://www.analog.com/" },
+      { name: "Espressif官网", href: "https://www.espressif.com/" },
+      { name: "SnapEDA", href: "https://www.snapeda.com/" },
+      { name: "立创开源硬件平台", href: "https://oshwhub.com/" },
+      { name: "EveryCircuit", href: "https://everycircuit.com/" },
+      { name: "TUV EMC", href: "https://www.tuv-lab.com/diancijianrongceshi/" }
     ]
   },
   {
-    title: "效率工具",
+    title: "云服务",
     links: [
-      { name: "DeepL", href: "https://www.deepl.com" },
-      { name: "Excalidraw", href: "https://excalidraw.com" },
-      { name: "Regex101", href: "https://regex101.com" }
+      { name: "阿里云", href: "https://www.aliyun.com/" },
+      { name: "腾讯云", href: "https://cloud.tencent.com/" },
+      { name: "Cloudflare", href: "https://www.cloudflare.com/" }
     ]
   }
 ];
